5c9f9aea059bcb9911e8fc3b445e147184e72a82
yhyuen <ben980322@gmail.com>
2023-07-03 23:40:28 +0800

    Updates

---
+++ _next/static/chunks/pages/events-4498372d8361fa17.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[695],{1442:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/events",function(){return t(8445)}])},8445:function(n,u,t){"use strict";t.r(u);var e=t(5893);t(7294),u.default=function(){return(0,e.jsx)("div",{children:"Events"})}}},function(n){n.O(0,[774,888,179],function(){return n(n.s=1442)}),_N_E=n.O()}]);
